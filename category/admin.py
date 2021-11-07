9d78bac912d4ab6134af8a601d4925ee87a914c4
prashantjaiswal647 <prashantjaiswal647@gmail.com>
2021-11-07 19:15:36 +0530

    Configuring Media File ,adding categories product, auto slug generation

--- category/admin.py
+++ category/admin.py
@@ -3,4 +3,8 @@ from .models import Category
 
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'slug','cat_image')
+
+admin.site.register(Category, CategoryAdmin)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT  = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
