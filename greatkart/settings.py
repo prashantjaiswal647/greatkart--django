6a15b77055f92948832c1b56e9dbf604d9bf7274
prashantjaiswal647 <prashantjaiswal647@gmail.com>
2021-12-04 17:32:18 +0530

    Switching Database to postgresql

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -92,10 +92,19 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'greatkart',
+        'USER':  'postgres',
+        'PASSWORD':  'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
+     }
+
 }
 
 
