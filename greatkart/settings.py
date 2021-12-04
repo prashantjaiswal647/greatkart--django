3d6a26ec45a4a5938e41f6bf6f270c14d4525753
prashantjaiswal647 <prashantjaiswal647@gmail.com>
2021-12-04 18:38:30 +0530

    Run Collectstatic

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '$5moqz&sfpd5v814l4#6rzzv#e+^=0eh-t#*0urq7^h-!=7!nz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
